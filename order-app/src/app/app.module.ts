c7afc028481c09c7b483e3eb2e79d7ddec6d64dc
Kaymon McCain <kaymonCodeM@outlook.com>
2022-11-22 14:10:52 -0600

    initialize backend and frontend

---
+++ order-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HnavComponent } from './hnav/hnav.component';
+import { VnavComponent } from './vnav/vnav.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HnavComponent,
+    VnavComponent,
+    DashboardComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
